995090b9c2deb0f3d0c53b6f2c1a59d4dde81e61
jasonord <jason.ord2004@gmail.com>
2024-09-02 01:29:00 -0400

    *ADDED* Functions
    Added functions that calculate weighted popularity scores based on release dates and recommends songs based on music features.

--- main.py
+++ main.py
@@ -108,3 +108,61 @@ music_df = get_trending_playlist_data(playlist_id, access_token)
 #prints dataframe with all of the columns
 pd.set_option('display.max_columns', None)
 print(music_df)
+#Function to calculate the weighted popularity scores based on their release
+def calculate_weighted_popularity(release_date):
+    # Convert the release date to datetime object
+    release_date = datetime.strptime(release_date, '%Y-%m-%d')
+
+    # Calculate the time span between release date and today's date
+    time_span = datetime.now() - release_date
+
+    # Calculate the weighted popularity score based on time span (the more recent, the more weighted)
+    weight = 1 / (time_span.days + 1)
+    return weight
+
+# Function for content-based recommendations based on music features
+def content_based_recommendations(input_song_name, num_recommendations = 5):
+    if input_song_name not in music_df['Track Name'].values:
+        print(f"'{input_song_name}' not found in the dataset. Please input a valid song name.")
+        return
+
+    # Gets index of the input song in the music DataFrame
+    input_song_index = music_df[music_df['Track Name'] == input_song_name].index[0]
+
+    # Calculates the similarity scores based on the musical features (cosine similarity)
+    similarity_scores = cosine_similarity([music_features_scaled[input_song_index]], music_features_scaled)
+
+    # Get the indices of the most similar songs EDIT
+    similar_song_indices = similarity_scores.argsort()[0][::-1][1:num_recommendations + 1]
+
+    # Get the names of the most similar songs based on the content-based filter
+    content_based_recommendations = music_df.iloc[similar_song_indices][['Track Name', 'Artists', 'Album Name', 'Release Date', 'Popularity']]
+
+    return content_based_recommendations
+
+def hybrid_recommendations(input_song_name, num_recommendations=5, alpha=0.5):
+    if input_song_name not in music_df['Track Name'].values:
+        print(f" '{input_song_name}' not found in the data set. Please input a valid song name.")
+        return
+
+    content_based_rec = content_based_recommendations(input_song_name, num_recommendations)
+
+    popularity_score = music_df.loc[music_df['Track Name'] == input_song_name, 'Popularity'].values[0]
+
+    weighted_popularity_score = popularity_score * calculate_weighted_popularity(music_df.loc[music_df['Track Name'] == input_song_name, 'Release Date'].values[0])
+
+    new_entry = pd.DataFrame({
+        'Track Name': [input_song_name],
+        'Artists': [music_df.loc[music_df['Track Name'] == input_song_name, 'Artists'].values[0]],
+        'Album name': [music_df.loc[music_df['Track Name'] == input_song_name, 'Album Name'].values[0]],
+        'Release Date': [music_df.loc[music_df['Track Name'] == input_song_name, 'Release Date'].values[0]],
+        'Popularity': [weighted_popularity_score]
+    })
+
+    hybrid_recommendations = pd.concat([content_based_rec, new_entry], ignore_index = True)
+
+    hybrid_recommendations = hybrid_recommendations.sort_values(by='Popularity', ascending = False)
+
+    hybrid_recommendations = hybrid_recommendations[hybrid_recommendations['Track Name'] != input_song_name]
+
+    return hybrid_recommendations
